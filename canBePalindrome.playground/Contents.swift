da9a64b381a1119652254243c38b3ca5c04feff1
jkaunert <44586402+jkaunert@users.noreply.github.com>
2019-09-04 00:14:05 -0500

    Updated README and added starter playground

---
+++ canBePalindrome.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func canBePalindrome(_ string: String) -> Bool {
+
+}
+
+
+// Test Cases
+canBePalindrome("tacocat")  // should return true
+canBePalindrome("daily")    // should return false
+canBePalindrome("a")        // should return true
+canBePalindrome("aa")       // should return true
+canBePalindrome("hannah")   // should return true
+canBePalindrome("abc")      // should return false
