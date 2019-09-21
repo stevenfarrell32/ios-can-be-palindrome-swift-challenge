3aff6c5d3d1edf554a5283012d1530e9506e43ed
Steven Farrell <stevenfarrelltraining@yahoo.com>
2019-09-20 21:53:14 -0400

    not finished

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,7 +1,11 @@
 import Foundation
 
-func canBePalindrome(_ string: String) -> Bool {
+enum myPalindromes {
+    case tacocat, daily, a, aa, hannah, abc
+}
 
+func canBePalindrome(_ string: String) -> Bool {
+    print(myPalindromes.tacocat)
 }
 
 
@@ -12,3 +16,30 @@ canBePalindrome("a")        // should return true
 canBePalindrome("aa")       // should return true
 canBePalindrome("hannah")   // should return true
 canBePalindrome("abc")      // should return false
+
+
+
+
+//var myPalindromes: [String : Bool] = ["tacocat" : true, "daily" : false, "a" : true, "aa" : true, "hannah" : true, "abc" : false]
+//
+//
+//func canBePalindrome(_ string: String) -> Bool {
+//
+//
+//    let case myPalindromes == true && myPalindromes.reversed() == String {
+//    print()
+//    }
+//
+//    // if the word printed, and the word printed in reverse is equal to the case, it should return the boolean value
+//    //first test the word printed
+//    //second check for the word reversed and printed
+//    //if else both cases are true, print the value
+//
+//}
+// Test Cases
+//canBePalindrome("tacocat")  // should return true
+//canBePalindrome("daily")    // should return false
+//canBePalindrome("a")        // should return true
+//canBePalindrome("aa")       // should return true
+//canBePalindrome("hannah")   // should return true
+//canBePalindrome("abc")      // should return false
