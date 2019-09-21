d74421387089e07ff4e6a4f96413e94639b71bdf
Steven Farrell <stevenfarrelltraining@yahoo.com>
2019-09-20 22:22:57 -0400

    working through it still

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,15 +1,22 @@
 import Foundation
 
-enum myPalindromes {
-    case tacocat, daily, a, aa, hannah, abc
-}
+var myPalindromes: [String : Bool] = ["tacocat" : true, "daily" : false, "a" : true, "aa" : true, "hannah" : true, "abc" : false]
+
 
 func canBePalindrome(_ string: String) -> Bool {
-    print(myPalindromes.tacocat)
+
+
+    let case myPalindromes[ : ] == myPalindromes.reversed() {
+    return
+    }
+
+    // if the word printed, and the word printed in reverse is equal to the case, it should return the boolean value
+    //first test the word printed
+    //second check for the word reversed and printed
+    //if else both cases are true, print the value
+
 }
-
-
-// Test Cases
+// test cases
 canBePalindrome("tacocat")  // should return true
 canBePalindrome("daily")    // should return false
 canBePalindrome("a")        // should return true
@@ -17,29 +24,24 @@ canBePalindrome("aa")       // should return true
 canBePalindrome("hannah")   // should return true
 canBePalindrome("abc")      // should return false
 
-
-
-
-//var myPalindromes: [String : Bool] = ["tacocat" : true, "daily" : false, "a" : true, "aa" : true, "hannah" : true, "abc" : false]
-//
+//enum myPalindromes {
+//    case tacocat, daily, a, aa, hannah, abc
+//}
 //
 //func canBePalindrome(_ string: String) -> Bool {
-//
-//
-//    let case myPalindromes == true && myPalindromes.reversed() == String {
-//    print()
-//    }
-//
-//    // if the word printed, and the word printed in reverse is equal to the case, it should return the boolean value
-//    //first test the word printed
-//    //second check for the word reversed and printed
-//    //if else both cases are true, print the value
-//
+//    print(myPalindromes.tacocat)
 //}
-// Test Cases
+//
+//
+//// Test Cases
 //canBePalindrome("tacocat")  // should return true
 //canBePalindrome("daily")    // should return false
 //canBePalindrome("a")        // should return true
 //canBePalindrome("aa")       // should return true
 //canBePalindrome("hannah")   // should return true
 //canBePalindrome("abc")      // should return false
+//
+//
+//
+//
+//
